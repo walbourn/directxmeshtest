141534e1b06f096cec1bb0fb44d31c97fb6d7175
walbourn <chuckw_walbourn@yahoo.com>
2022-11-23 17:05:36 -0800

    Clean up

--- vb/vb12.cpp
+++ vb/vb12.cpp
@@ -67,27 +67,6 @@ namespace
         { XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT2(1.0f, 0.0f) },
     };
 
-    const uint16_t s_cubeIB[] =
-    {
-        3,1,0,
-        2,1,3,
-
-        6,4,5,
-        7,4,6,
-
-        11,9,8,
-        10,9,11,
-
-        14,12,13,
-        15,12,14,
-
-        19,17,16,
-        18,17,19,
-
-        22,20,21,
-        23,20,22
-    };
-
     const D3D12_INPUT_ELEMENT_DESC s_cubeIL[] =
     {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
