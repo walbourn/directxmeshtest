7faeb3b524f047a238a31c33ee4c7035ca1ea572
Chuck Walbourn <walbourn@users.noreply.github.com>
2024-10-28 15:13:07 -0700

    Update test scripts for GNU-style long options (#21)

--- toolscripts/meshconvert/t-basics.cmd
+++ toolscripts/meshconvert/t-basics.cmd
@@ -7,21 +7,26 @@ echo -nologo -o %OUTTESTDIR%\cup.sdkmesh %REFDIR%\cup._obj
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
 
 echo.
-echo -nologo -sdkmesh -o %OUTTESTDIR%\cup2.sdkmesh %REFDIR%\cup._obj
-"%RUN%" -nologo -sdkmesh -o %OUTTESTDIR%\cup2.sdkmesh %REFDIR%\cup._obj
+echo -nologo -ft sdkmesh -o %OUTTESTDIR%\cup2.sdkmesh %REFDIR%\cup._obj
+"%RUN%" -nologo -ft sdkmesh -o %OUTTESTDIR%\cup2.sdkmesh %REFDIR%\cup._obj
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
 
 echo.
-echo -nologo -sdkmesh2 -o %OUTTESTDIR%\cupPBR.sdkmesh %REFDIR%\cup._obj
-"%RUN%" -nologo -sdkmesh2 -o %OUTTESTDIR%\cupPBR.sdkmesh %REFDIR%\cup._obj
+echo -nologo -ft sdkmesh2 -o %OUTTESTDIR%\cupPBR.sdkmesh %REFDIR%\cup._obj
+"%RUN%" -nologo -ft sdkmesh2 -o %OUTTESTDIR%\cupPBR.sdkmesh %REFDIR%\cup._obj
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
 
 echo.
-echo -nologo -cmo -o %OUTTESTDIR%\cup.cmo %REFDIR%\cup._obj
-"%RUN%" -nologo -cmo -o %OUTTESTDIR%\cup.cmo %REFDIR%\cup._obj
+echo -nologo -ft cmo -o %OUTTESTDIR%\cup.cmo %REFDIR%\cup._obj
+"%RUN%" -nologo -ft cmo -o %OUTTESTDIR%\cup.cmo %REFDIR%\cup._obj
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
 
 echo.
-echo -nologo -vbo -o %OUTTESTDIR%\cup.vbo %REFDIR%\cup._obj
-"%RUN%" -nologo -vbo -o %OUTTESTDIR%\cup.vbo %REFDIR%\cup._obj
+echo -nologo -ft vbo -o %OUTTESTDIR%\cup.vbo %REFDIR%\cup._obj
+"%RUN%" -nologo -ft vbo -o %OUTTESTDIR%\cup.vbo %REFDIR%\cup._obj
+if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
+
+echo.
+echo -nologo -ft obj -o %OUTTESTDIR%\cup2._obj %REFDIR%\cup._obj
+"%RUN%" -nologo -ft obj -o %OUTTESTDIR%\cup2._obj %REFDIR%\cup._obj
 if %ERRORLEVEL% NEQ 0 ( call failure.cmd "Failed" )
